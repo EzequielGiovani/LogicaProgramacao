5cce6b32b3327ec87c2ac39a7d5a4710fd3ad334
EzequielGiovani <168599421+EzequielGiovani@users.noreply.github.com>
2026-06-17 22:15:13 -0300

    primeira funcao 0617_funcoes

---
+++ 0617_funcoes/primeira_funcao.py
@@ -0,0 +1,8 @@
+def somar_valores(parametro1, parametro2):
+    """Docstring: descreve oque a funcao faz"""
+    # Corpo da função
+    resultado = parametro1 + parametro2
+    return resultado
+
+soma = somar_valores(2, 3)
+print(soma)
