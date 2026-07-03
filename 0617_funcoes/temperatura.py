aad38389df17f3f11ea9bc80aeacc9fdd7b66e2f
Ezequiel Santos <168599421+EzequielGiovani@users.noreply.github.com>
2026-07-03 10:27:38 -0300

    Delete temperatura.py

--- 0617_funcoes/temperatura.py
+++
@@ -1,8 +0,0 @@
-def fahrenheit_para_celsius(f):
-    """Converte teperatura de Farenheit para Celsius"""
-    celsius = (f - 32) * 5 / 9
-    return celsius
-
-temp = 98
-celsius = fahrenheit_para_celsius(temp)
-print(celsius)
