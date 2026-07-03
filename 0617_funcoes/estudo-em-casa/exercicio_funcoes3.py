cabfae5da6c04c15a407d6ec240c78b2ccc370bd
Ezequiel Santos <168599421+EzequielGiovani@users.noreply.github.com>
2026-07-03 11:38:19 -0300

    Create exercicio_funcoes3.py

---
+++ 0617_funcoes/estudo-em-casa/exercicio_funcoes3.py
@@ -0,0 +1,10 @@
+numero = float(input(f"DIgite o numero: "))
+
+def par_ou_impar(valor):
+    numero
+    if numero % 2 == 0:
+        print(f"{numero} é par!")
+    else:
+        print(f"{numero} é impar!")
+
+par_ou_impar(numero)
