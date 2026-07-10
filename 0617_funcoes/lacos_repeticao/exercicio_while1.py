c7adbe49bb42cdcb554fa5f66339c2c5482f4ac5
Ezequiel Santos <168599421+EzequielGiovani@users.noreply.github.com>
2026-07-10 17:05:19 -0300

    Create exercicio_while1.py

---
+++ 0617_funcoes/lacos_repeticao/exercicio_while1.py
@@ -0,0 +1,9 @@
+# EXIBIR NÚMEROS DE UM A DEZ
+
+lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numeros = 0
+
+while numeros < len(lista_numeros):
+    print(lista_numeros[numeros])
+    numeros += 1
