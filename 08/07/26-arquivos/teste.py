46a6274f0baab175897defd75bb3835cb8d9fdb3
EzequielGiovani <rafael.k@aluno.senai.br>
2026-07-10 21:09:19 -0300

    Delete teste.py

--- 08/07/26-arquivos/teste.py
+++
@@ -1,9 +0,0 @@
-nomes = []
-
-for nome in range(3):
-    nome = input(f"Digite um nome: ")
-    nomes.append(nome + "\n")
-    with open("nomes.txt", "w") as f:
-        f.writelines(nomes)
-
-print(nomes)
