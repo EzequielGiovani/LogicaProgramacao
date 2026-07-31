930064b31d1e7c4874fec6ae5eea7d79dfa7c89e
ikegame2019 <ikejd.2018@gmail.com>
2026-07-31 20:32:36 -0300

    Create exercicio1-IMC.py

---
+++ 0731/exercicio1-IMC.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from tkinter import messagebox
+
+janela = tk.Tk()
+
+janela.title("Calcular IMC")
+janela.geometry("800x600")
+
+peso_label = tk.Label(janela, text="Peso(kg):")
+peso_label.pack()
+
+peso_entrada = tk.Entry(janela)
+peso_entrada.pack()
+
+altura_label = tk.Label(janela, text="Altura(m):")
+altura_label.pack()
+
+altura_entrada = tk.Entry(janela)
+altura_entrada.pack()
+
+resultado = tk.Label(janela, text="")
+resultado.pack()
+
+def calcular_imc():
+    peso = float(peso_entrada.get())
+    altura = float(altura_entrada.get())
+
+    imc = peso / (altura * altura)
+
+    if imc < 18.5:
+        classificacao = "Abaixo do peso"
+    elif imc < 25:
+        classificacao = "Saudável"
+    elif imc < 30:
+        classificacao = "Sobrepeso"
+    else:
+        classificacao = "Obeso"
+
+    resultado.config(
+        janela,
+        text=f"Seu IMC é: {imc:.2f} \nClassificao: {classificacao}"
+    )
+
+botao_calcular = tk.Button(
+    janela,
+    text="Calcular",
+    command=calcular_imc
+    )
+
+botao_calcular.pack()
+janela.mainloop()
